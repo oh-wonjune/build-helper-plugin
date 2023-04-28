9d7b474cb650966119e73f4965726ecad7786f54
오원준 <kbw0309@douzone.com>
2023-04-28 21:20:34 +0900

    git branch

--- src/index.js
+++ src/index.js
@@ -1,50 +1,54 @@
-const {Compiler} = require('webpack');
-const {execSync} = require('child_process');
-const path = require('path');
+const { Compiler } = require("webpack");
+const { execSync } = require("child_process");
+const path = require("path");
 
 class BuildHelperPlugin {
-    constructor(prod_Branch, dev_Branch) {
-        this.prod_Branch = prod_Branch;
-        this.dev_Branch = dev_Branch;
-    }
+  constructor(prod_Branch, dev_Branch) {
+    this.prod_Branch = prod_Branch;
+    this.dev_Branch = dev_Branch;
+  }
 
-    apply(compiler) {
-        const modifiedFiles = getModifiedFiles();
+  apply(compiler) {
+    const modifiedFiles = this.getModifiedFiles();
 
-        compiler.plugin('emit', (compilation, callback) => {
+    compiler.plugin("emit", (compilation, callback) => {
+      const assetsToBuild = new Set(modifiedFiles);
+      // 변경된 파일의 종속성을 찾습니다.
+      compilation.modules.forEach((module) => {
+        if (module.resource && modifiedFiles.includes(module.resource)) {
+          assetsToBuild.add(module.resource);
+          module.reasons.forEach((reason) => {
+            if (reason.module && reason.module.resource) {
+              assetsToBuild.add(reason.module.resource);
+            }
+          });
+        }
+      });
 
-            const assetsToBuild = new Set(modifiedFiles);
-            // 변경된 파일의 종속성을 찾습니다.
-            compilation.modules.forEach((module) => {
-                if (module.resource && modifiedFiles.includes(module.resource)) {
-                    assetsToBuild.add(module.resource);
-                    module.reasons.forEach((reason) => {
-                        if (reason.module && reason.module.resource) {
-                            assetsToBuild.add(reason.module.resource);
-                        }
-                    });
-                }
-            });
+      // 변경된 파일과 그 종속성만 빌드하도록 웹팩 빌드를 조절합니다.
+      compilation.assets = Object.keys(compilation.assets)
+        .filter((asset) => assetsToBuild.has(asset))
+        .reduce((acc, file) => {
+          acc[file] = compilation.assets[file];
+          return acc;
+        }, {});
 
-            // 변경된 파일과 그 종속성만 빌드하도록 웹팩 빌드를 조절합니다.
-            compilation.assets = Object.keys(compilation.assets)
-                .filter((asset) => assetsToBuild.has(asset))
-                .reduce((acc, file) => {
-                    acc[file] = compilation.assets[file];
-                    return acc;
-                }, {});
+      callback();
+    });
+  }
 
-            callback();
-        });
-    }
-}
+  getModifiedFiles() {
+    const output = execSync(
+      `git diff --name-only ${this.prod_Branch} ${this.dev_Branch}`
+    ).toString();
 
-function getModifiedFiles() {
-    const output = execSync('git diff --name-only '+this.prod_Branch+' '+ this.dev_Branch).toString();
-    const relativePaths = output.trim().split('\n');
+    const relativePaths = output.trim().split("\n");
     const projectRoot = process.cwd();
 
-    return relativePaths.map((relativePath) => path.resolve(projectRoot, relativePath));
+    return relativePaths.map((relativePath) =>
+      path.resolve(projectRoot, relativePath)
+    );
+  }
 }
 
 module.exports = BuildHelperPlugin;
